7b525a977eba803ad97fa390f434ea371b79ef1c
filipblyckert <112581641+filipblyckert@users.noreply.github.com>
2023-11-13 13:55:59 +0100

    komplettering

---
+++ Controller/ContactsController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Umbraco.Cms.Core.Cache;
+using Umbraco.Cms.Core.Logging;
+using Umbraco.Cms.Core.Routing;
+using Umbraco.Cms.Core.Services;
+using Umbraco.Cms.Core.web;
+using Umbraco.Cms.Infrastructure.Persistence;
+using Umbraco.Cms.Web.Wesite.ActionResults;
+using Umbraco.Cms.Web.Website.Controllers;
+
+public class ContactsController : SurfaceController
+{
+	public ContactsController(IUmbracoContextAccessor umbracoContextAccessor, IUmbracoDatabaseFactory databaseFactory, ServiceContext services, AppCaches appcaches, IProfilingLogger, profilingLogger, IPublishedUrlProvider publishedUrlProvider) : base(umbracoContextAccessor, databaseFactory, services, appcaches, profilingLogger, publishedUrlProvider)
+	{
+	}
+
+	[HttpPost]
+	public async Task<IActionResult> Index(ContactForm contactForm)
+	{
+		if (!ModelState.IsValid)
+			return CurrentUmbracoPage();
+
+		using var mail = new MailService();
+
+		await mail.SendAsync(contactForm.Email, "Your request was received" "Hi, your request was received");
+
+
+		await mail.SendAsync("");
+
+        return LocalRedirect(contactForm.RedirectUrl ?? "/");
+	}
+}
